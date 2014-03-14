82f4dc58464ff696ec9fbb65ac37d0453a592058
Daniel Kronovet <kronovet@gmail.com>
2014-03-14 16:10:31 -0400

    built blog platform according to assessment spec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsAssessment::Application.config.secret_key_base = '9f3c24dd65f1a9a8ecc9dfe0d5e32693046caac2b89f792124ce9f5cdce64603ed52719ec3c0b49629a89ed6d24cf0688e66c3d049a912f6e997c9ee295b9f01'
